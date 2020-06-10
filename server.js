9fd65a9396915e21bb34a25afdc1e13a572c7a27
2012b <2012bhandari.ha@gmail.com>
2020-06-10 22:27:15 +0100

    'minor'

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const path=require('path')
 const app=express();
+const dotenv=require('dotenv')
 const db=require('./utils/db')
 app.use(express.json());
+dotenv.config({path:'./config.env'})
 
 //port
 db();
-const PORT=5000
+const PORT=process.env.PORT||5000
 const NODE_ENV='production'
 //routes
 app.use('/api',require('./routes/tech'))
