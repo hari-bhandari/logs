59d5706f56a0186bceb49daa5904905fd48b0edc
2012b <2012bhandari.ha@gmail.com>
2020-06-10 22:18:57 +0100

    fixed minor errors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express');
-
+const path=require('path')
 const app=express();
 const db=require('./utils/db')
 app.use(express.json());
@@ -7,9 +7,14 @@ app.use(express.json());
 //port
 db();
 const PORT=5000
-
+const NODE_ENV='production'
 //routes
-app.use('/',require('./routes/tech'))
-app.use('/',require('./routes/logs'))
-//server static ass
+app.use('/api',require('./routes/tech'))
+app.use('/api',require('./routes/logs'))
+//server static asset
+if(NODE_ENV ==='production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+}
+
 app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
