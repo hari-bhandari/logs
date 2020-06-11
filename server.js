ea75865bd84f0926927294260a7b30819fc2abc0
2012b <2012bhandari.ha@gmail.com>
2020-06-11 10:19:43 +0100

    'v1.03'

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ dotenv.config({path:'./config.env'})
 //port
 db();
 const PORT=process.env.PORT||5000
-const NODE_ENV='production'
 //routes
 app.use('/api',require('./routes/tech'))
 app.use('/api',require('./routes/logs'))
 //server static asset
-if(NODE_ENV ==='production'){
+if(process.env.NODE_ENV ==='production'){
     app.use(express.static('client/build'))
     app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')))
 }
